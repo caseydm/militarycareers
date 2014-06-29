0d9d35cab37ff392a17dc05545b698581547b846
Casey M <caseym@gmail.com>
2014-06-29 13:32:52 -0400

    added form

--- app/controllers/careers_controller.rb
+++ app/controllers/careers_controller.rb
@@ -1,8 +1,21 @@
 class CareersController < ApplicationController
   def new
+  	@career = Career.new
   end
 
   def show
   	@career = Career.find(params[:id])
   end
+
+  def create
+    @career = Career.new(career_params)
+    @career.save
+    redirect_to @career
+  end
+
+  private
+
+    def career_params
+      params.require(:career).permit(:name, :school_length)
+    end
 end
