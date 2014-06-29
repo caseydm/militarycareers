38b0b0f96d623f5a18aace177246a82d31809804
Casey M <caseym@gmail.com>
2014-06-28 22:45:05 -0400

    added new controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
+  root  'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 end
